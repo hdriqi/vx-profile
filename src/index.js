60ca65a011a30186985ca34295471579b5f6d83f
hdriqi <ra@evius.id>
2020-03-31 11:34:41 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const Database = require('./db')
 const Ctl = require('./controllers')
@@ -19,6 +20,7 @@ const init = async () => {
 
   const app = express()
 
+  app.use(cors())
   app.use(bodyParser.urlencoded({ extended: false }))
   app.use(bodyParser.json())
 
